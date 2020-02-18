bed16e7ea8d3db94c2545ff743fa13fdef2626ab
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-18 10:44:11 -0600

    can register user with response

--- traval-buuds-app/src/App.js
+++ traval-buuds-app/src/App.js
@@ -4,16 +4,39 @@ import LoginRegister from './LoginRegister'
 import { Header } from  'semantic-ui-react'
 
 export default class App extends Component{
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
 
     this.state = {
         status: 'register'
     }
   }
 
-  register = (registerInfo) => {
+  register = async (registerInfo) => {
+    
+    try {
     console.log('You made it to register func, here is the reg info', registerInfo );
+    const url = process.env.REACT_APP_API_URL + '/api/v1/users/register' 
+
+    const registerResponse = await fetch(url, {
+      credentials: 'include',
+      method: 'POST',
+      body: JSON.stringify(registerInfo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    console.log('registerInfo response', registerResponse);
+
+    const registerJson = await registerResponse.json()
+
+    console.log(registerJson);
+
+    }catch(err){
+      console.log(err);
+    }
+      
   }
   render() {
 
