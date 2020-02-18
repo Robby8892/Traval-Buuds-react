4ec5b34d4ae1f2c106d6a38f589d34bb9a06f17d
roberto cortes <robertcortesgrimm223@gmail.com>
2020-02-18 09:30:49 -0600

    created login/register component

--- traval-buuds-app/src/App.js
+++ traval-buuds-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import LoginRegister from './LoginRegister'
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <LoginRegister/>
     </div>
   );
 }
